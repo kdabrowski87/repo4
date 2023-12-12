9b7c5aa15d6f03f63e21a50d7e2163fb10dc0486
Krzysztof Dabrowski <k.dabrowski87@gmail.com>
2023-12-12 19:02:35 +0100

    initial

---
+++ src/main/java/com/kd1987/rest/webservices/javarestapi1/JavaRestApi1Application.java
@@ -0,0 +1,13 @@
+package com.kd1987.rest.webservices.javarestapi1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaRestApi1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaRestApi1Application.class, args);
+	}
+
+}
