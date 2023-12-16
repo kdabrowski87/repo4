e1fe94dc66d3bfd0d81781956b0fdbfd36e7232a
Krzysztof Dabrowski <k.dabrowski87@gmail.com>
2023-12-16 11:39:52 +0100

    validation plus swagger

--- src/main/java/com/kd1987/rest/webservices/javarestapi1/exception/CustomizedResponseEntityExceptionHandler.java
+++ src/main/java/com/kd1987/rest/webservices/javarestapi1/exception/CustomizedResponseEntityExceptionHandler.java
@@ -1,8 +1,11 @@
 package com.kd1987.rest.webservices.javarestapi1.exception;
 
 import com.kd1987.rest.webservices.javarestapi1.user.UserNotFoundException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -27,6 +30,20 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
                 ex.getMessage(),request.getDescription(false));
 
         return new ResponseEntity<ErrorDetails>(errorDetails, HttpStatus.NOT_FOUND);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+
+        ErrorDetails errorDetails = new ErrorDetails(LocalDateTime.now(),
+                "Total Errors: " + ex.getErrorCount() +  "First error: "
+                        + ex.getFieldError().getDefaultMessage()
+                ,request.getDescription(false));
+
+        return new ResponseEntity(errorDetails, HttpStatus.BAD_REQUEST);
+
+
+        //return this.handleExceptionInternal(ex, (Object)null, headers, status, request);
     }
 
 }
